37c5f2f7f0c9cad5795170be1a73dfeff779da2c
Diegoss32 <d.sousa.santos@uni9.edu.br>
2025-04-24 21:53:42 +0000

    adicionando uma classe produto

---
+++ Produto.cs
@@ -0,0 +1,9 @@
+public class Produto{
+public string? Nome { get; set;}
+public int codigoProduto{get; set;}
+public int quantidade{get; set;}
+
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Produto produto = new Produto();
+
+Console.Write("digite o nome do produto: ");
+produto.Nome = Console.ReadLine();

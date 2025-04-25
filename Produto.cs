a4ecb10b8efd82dd41839caaeee2c7a684a54761
Diegoss32 <d.sousa.santos@uni9.edu.br>
2025-04-25 00:04:28 +0000

    Cadastro de produtos

--- Produto.cs
+++ Produto.cs
@@ -2,8 +2,65 @@ public class Produto{
 public string? Nome { get; set;}
 public int codigoProduto{get; set;}
 public int quantidade{get; set;}
+public double preco{get; set;}
+
+public Produto(){}
 
 
+public Produto(string nome, int codigoProduto, int quantidade, double preco)
+{
+    Nome = nome;
+    this.codigoProduto = codigoProduto;
+    this.quantidade = quantidade;
+    this.preco = preco;
 }
+public double valorTotal (){
+    return quantidade * preco;
+}
+public void AdicionaProduto(int quantidadeAdicional)
+{
+    if (quantidadeAdicional > 0)
+    {
+        quantidade += quantidadeAdicional;
+        Console.WriteLine($"{quantidadeAdicional} unidades adicionadas ao produto '{Nome}'. Quantidade atual: {quantidade}");
+    }
+    else
+    {
+        Console.WriteLine("Quantidade inválida para adicionar.");
+    }
+}
+public static Produto[] removeProduto(Produto[] produtos, int codigoParaRemover)
+{
+    List<Produto> listaAtualizada = new List<Produto>();
 
+    foreach (var produto in produtos)
+    {
+        if (produto.codigoProduto != codigoParaRemover)
+        {
+            listaAtualizada.Add(produto);
+        }
+    }
 
+    return listaAtualizada.ToArray();  // <- retorno garantido!
+}
+public void RemoverQuantidade(int quantidadeParaRemover)
+{
+    if (quantidadeParaRemover <= 0)
+    {
+        Console.WriteLine("Quantidade inválida.");
+    }
+    else if (quantidadeParaRemover > quantidade)
+    {
+        Console.WriteLine($"Quantidade insuficiente. Estoque atual: {quantidade}");
+    }
+    else
+    {
+        quantidade -= quantidadeParaRemover;
+        Console.WriteLine($"Removido {quantidadeParaRemover} unidade(s) do produto '{Nome}'. Estoque atual: {quantidade}");
+    }
+}
+public override string ToString()
+{
+    return $"Nome: {Nome}, Código: {codigoProduto}, Quantidade: {quantidade}, Preço:R$ {preco:C}, Valor Total:R$ {valorTotal():C}";
+}
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,119 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Produto produto = new Produto();
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-Console.Write("digite o nome do produto: ");
-produto.Nome = Console.ReadLine();
+Produto p = new Produto();
+Console.WriteLine("Bem-vindo ao sistema de gerenciamento de produtos!");
+Console.Write("digite quantos produtos ira cadastra: ");
+int n= int.Parse(Console.ReadLine()!);
+Produto[] produtos = new Produto[n];
+for(int i = 0; i < n; i++){
+    Console.Write("digite o nome do produto: ");
+    string? Nome = Console.ReadLine();
+
+    Console.Write("digite o codigo do produto: ");
+    int codigoProduto = int.Parse(Console.ReadLine()!);
+
+    Console.Write("digite a quantidade do produto: ");
+    int quantidade = int.Parse(Console.ReadLine()!);    
+
+    Console.Write("digite o preco do produto: ");
+    double preco = double.Parse(Console.ReadLine()!);   
+
+    produtos[i] = new Produto(Nome, codigoProduto, quantidade, preco);
+    Console.WriteLine($"Produto {i + 1} cadastrado com sucesso!");
+    Console.WriteLine("--------------------------");
+}
+
+Console.WriteLine("\nLista de produtos cadastrados:");
+foreach (var produto in produtos)
+{
+    Console.WriteLine(produto);
+}
+
+
+Console.WriteLine("--------------------------");
+while (true)
+{
+        Console.WriteLine("Menu:");
+        Console.WriteLine("1 - Adicionar produto ao estoque");
+        Console.WriteLine("2 - Remover produto inteiro");
+        Console.WriteLine("3 - Remover quantidade de produto");
+        Console.WriteLine("4 - Exibir produtos");
+        Console.WriteLine("5 - Sair");
+        Console.Write("Escolha uma opção: ");
+        int opcao = int.Parse(Console.ReadLine()!);
+
+    if (opcao == 1)
+    {
+        Console.Write("Digite o código do produto para adicionar ao estoque: ");
+        int codigo = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Digite a quantidade a ser adicionada: ");
+        int quantidadeAdicionar = int.Parse(Console.ReadLine()!);
+
+       
+        foreach (var produto in produtos)
+        {
+            if (produto.codigoProduto == codigo)
+            {
+                produto.AdicionaProduto(quantidadeAdicionar);
+                break;
+            }
+        }
+    }
+    else if (opcao == 2)
+    {
+        Console.Write("Digite o código do produto que deseja remover: ");
+        int codigoRemover = int.Parse(Console.ReadLine()!);
+
+        produtos = Produto.removeProduto(produtos, codigoRemover);
+    }
+    else if (opcao == 3)
+    {
+        Console.Write("Digite o código do produto: ");
+        int codigo = int.Parse(Console.ReadLine()!);
+
+        Console.Write("Digite a quantidade a ser removida: ");
+        int quantidade = int.Parse(Console.ReadLine()!);
+
+        
+        foreach (var produto in produtos)
+        {
+            if (produto.codigoProduto == codigo)
+            {
+                produto.RemoverQuantidade(quantidade);
+                break;
+            }
+        }
+    }
+    else if (opcao == 4)
+    {
+        
+        Console.WriteLine("\nLista de produtos cadastrados:");
+        foreach (var produto in produtos)
+        {
+            Console.WriteLine(produto);
+        }
+    }
+    else if (opcao == 5)
+    {
+        break; 
+    }
+    else
+    {
+        Console.WriteLine("Opção inválida. Tente novamente.");
+    }
+
+    Console.WriteLine(); 
+}
+
+
+Console.WriteLine("\nLista de produtos cadastrados:");
+foreach (var produto in produtos)
+{
+    Console.WriteLine(produto);
+}
